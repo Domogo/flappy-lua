1ee682de2000816423b6d67d5b0b109682169809
Domogo <domsatrapa@gmail.com>
2024-12-30 10:32:52 +0100

    bird becomes flappy

--- main.lua
+++ main.lua
@@ -32,6 +32,8 @@ function love.load()
         fullscreen = false,
         resizable = true
     })
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.resize(w, h)
@@ -39,6 +41,8 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+
     if key == "escape" then
         love.event.quit()
     end
@@ -49,6 +53,17 @@ function love.update(dt)
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
     bird:update(dt)
+
+    love.keyboard.keysPressed = {}
+end
+
+-- wow I lovehate this
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
 end
 
 function love.draw()
